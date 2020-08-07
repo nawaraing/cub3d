e7708e3757730f5ddd3ef80a0ad6f92233eaf039
junkang <nawaddaing@gmail.com>
2020-08-08 06:47:42 +0900

    Create some tests

--- mlx_test/circle.c
+++ mlx_test/circle.c
@@ -1,5 +1,5 @@
 #include "../minilibx_mms_20200219/mlx.h"
-
+#include <math.h>
 //compile command is 
 //
 // gcc main.c -lmlx -framework OpenGL -framework AppKit
@@ -12,20 +12,24 @@ typedef struct	s_data {
 	int		endian;
 }				t_data;
 
-/*
-int		deal_key(int key, void *param)
-{
-	write(1, "press key\n", 10);
-	return (0);
-}
-*/
-
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
 	char *dst;
 
 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
 	*(unsigned int *)dst = color;
+}
+
+void	draw_circle(int x, int y, int r, int color, t_data *data)
+{
+	for (int i = y - r; i <= y + r; i++)
+	{
+		for (int j = x - r; j <= x + r; j++)
+		{
+			if (pow((x - j), 2) + pow((y - i), 2) <= pow(r, 2))
+				my_mlx_pixel_put(data, j, i, color);
+		}
+	}
 }
 
 int		main(void)
@@ -36,11 +40,9 @@ int		main(void)
 
 	mlx_ptr = mlx_init();
 	win_ptr = mlx_new_window(mlx_ptr, 1920, 1080, "mlx 42");
-//	mlx_pixel_put(mlx_ptr, win_ptr, 250, 250, 0xFFFFFF);
-//	mlx_key_hook(win_ptr, deal_key, (void *)0);
 	data.img = mlx_new_image(mlx_ptr, 1920, 1080);
 	data.addr = mlx_get_data_addr(data.img, &data.bits_per_pixel, &data.line_length, &data.endian);
-	my_mlx_pixel_put(&data, 5, 5, 0xFFFFFF);
+	draw_circle(1920 / 2, 1080 / 2 - 200, 270, 0xF0F0, &data);
 	mlx_put_image_to_window(mlx_ptr, win_ptr, data.img, 0, 0);
 	mlx_loop(mlx_ptr);
 	return (0);
--- mlx_test/rainbow.c
+++ mlx_test/rainbow.c
@@ -1,5 +1,5 @@
 #include "../minilibx_mms_20200219/mlx.h"
-
+#include <math.h>
 //compile command is 
 //
 // gcc main.c -lmlx -framework OpenGL -framework AppKit
@@ -12,20 +12,32 @@ typedef struct	s_data {
 	int		endian;
 }				t_data;
 
-/*
-int		deal_key(int key, void *param)
-{
-	write(1, "press key\n", 10);
-	return (0);
-}
-*/
-
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
 	char *dst;
 
 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
 	*(unsigned int *)dst = color;
+}
+
+int		ft_get_color(int y, int height)
+{
+	if (y < height / 7
+
+}
+
+void	draw_rainbow(int x, int y, t_data *data)
+{
+	int		color;
+
+	for (int i = 1080 / 2 - y, i <= 1080 / 2 + y; i++)
+	{
+		for (int j = 1920 / 2 - x, i <= 1920 / 2 + x, j++)
+		{
+			color = ft_get_color(i, 2 * y);
+			my_mlx_pixel_put(data, j, i, color);
+		}
+	}
 }
 
 int		main(void)
@@ -36,11 +48,9 @@ int		main(void)
 
 	mlx_ptr = mlx_init();
 	win_ptr = mlx_new_window(mlx_ptr, 1920, 1080, "mlx 42");
-//	mlx_pixel_put(mlx_ptr, win_ptr, 250, 250, 0xFFFFFF);
-//	mlx_key_hook(win_ptr, deal_key, (void *)0);
 	data.img = mlx_new_image(mlx_ptr, 1920, 1080);
 	data.addr = mlx_get_data_addr(data.img, &data.bits_per_pixel, &data.line_length, &data.endian);
-	my_mlx_pixel_put(&data, 5, 5, 0xFFFFFF);
+	draw_rainbow(1920 / 2, 1080 / 2, &data);
 	mlx_put_image_to_window(mlx_ptr, win_ptr, data.img, 0, 0);
 	mlx_loop(mlx_ptr);
 	return (0);
