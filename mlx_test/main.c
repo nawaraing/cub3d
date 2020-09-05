a819cdfaaeb094f7b076a23257f1b39b29fba811
강준현 <kangjunhyeon@gangjunhyeon-ui-MacBookPro.local>
2020-09-05 22:04:56 +0900

    Update main.c

--- mlx_test/main.c
+++ mlx_test/main.c
@@ -1,47 +1,100 @@
-#include "../minilibx_mms_20200219/mlx.h"
+#include "main.h"
+#include <stdio.h>
 
-//compile command is 
-//
-// gcc main.c -Imlx -framework OpenGL -framework AppKit
-
-typedef struct	s_data {
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}				t_data;
-
-/*
-int		deal_key(int key, void *param)
+void     	       my_mlx_pixel_put(t_img *data, int x, int y, int color)
 {
-	write(1, "press key\n", 10);
+    char    *dst;
+
+    dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+    *(unsigned int*)dst = color;
+}
+
+void			ft_draw_circle(t_img *img, int x, int y, int r, int color)
+{
+	for (int i = x - r; i < x + r; i++)
+	{
+		for (int j = y - r; j < y + r; j++)
+			if (pow(x - i, 2) + pow(y - j, 2) <= r * r)
+				my_mlx_pixel_put(img, i, j, color);
+	}
+	return ;
+}
+
+int			ft_key_press(int keycode, t_cub *cub)
+{
+	if (keycode == KEY_W || keycode == KEY_UP)
+		cub->keyboard.up = 1;
+	else if (keycode == KEY_A || keycode == KEY_LEFT)
+		cub->keyboard.left = 1;
+	else if (keycode == KEY_S || keycode == KEY_DOWN)
+		cub->keyboard.down = 1;
+	else if (keycode == KEY_D || keycode == KEY_RIGHT)
+		cub->keyboard.right = 1;
 	return (0);
 }
-*/
 
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+int			ft_key_release(int keycode, t_cub *cub)
 {
-	char *dst;
-
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-	*(unsigned int *)dst = color;
+	if (keycode == KEY_ESC)
+//		mlx_destroy_window(cub->ptr.mlx, cub->ptr.win);
+		exit(0);
+	else if (keycode == KEY_W || keycode == KEY_UP)
+		cub->keyboard.up = 0;
+	else if (keycode == KEY_A || keycode == KEY_LEFT)
+		cub->keyboard.left = 0;
+	else if (keycode == KEY_S || keycode == KEY_DOWN)
+		cub->keyboard.down = 0;
+	else if (keycode == KEY_D || keycode == KEY_RIGHT)
+		cub->keyboard.right = 0;
+	return (0);
 }
 
-int		main(void)
+void			put_img(t_cub *cub)
 {
-	void	*mlx_ptr;
-	void	*win_ptr;
-	t_data	data;
+	t_ptr			*win;
+	t_img			*img;
 
-	mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr, 1920, 1080, "mlx 42");
-//	mlx_pixel_put(mlx_ptr, win_ptr, 250, 250, 0xFFFFFF);
-//	mlx_key_hook(win_ptr, deal_key, (void *)0);
-	data.img = mlx_new_image(mlx_ptr, 1920, 1080);
-	data.addr = mlx_get_data_addr(data.img, &data.bits_per_pixel, &data.line_length, &data.endian);
-	my_mlx_pixel_put(&data, 5, 5, 0xFFFFFF);
-	mlx_put_image_to_window(mlx_ptr, win_ptr, data.img, 0, 0);
-	mlx_loop(mlx_ptr);
+	win = &cub->ptr;
+	img = &cub->img;
+	mlx_put_image_to_window(win->mlx, win->win, img->img, 0, 0);
+}
+
+int			ft_main_loop(t_cub *cub)
+{
+	if (cub->keyboard.up == 1)
+		cub->circle.y += 1;
+	else if (cub->keyboard.down == 1)
+		cub->circle.y -= 1;
+	else if (cub->keyboard.left == 1)
+		cub->circle.x -= 1;
+	else if (cub->keyboard.right == 1)
+		cub->circle.x += 1;
+	else
+		return (0);
+	ft_draw_circle(&cub->img, cub->circle.x, cub->circle.y, 100, 0x00FF0000);
+//	mlx_put_image_to_window(cub->ptr.mlx, cub->ptr.win, cub->img.img, 0, 0);
+	put_img(cub);
+	return (0);
+}
+
+void			ft_init_circle(t_circle *circle)
+{
+	circle->x = 576;
+	circle->y = 360;
+}
+
+int			main(void)
+{
+	t_cub		cub;
+
+	cub.ptr.mlx = mlx_init();
+	cub.ptr.win = mlx_new_window(cub.ptr.mlx, 1152, 720, "getting_start");
+	cub.img.img = mlx_new_image(cub.ptr.mlx, 1152, 720);
+	cub.img.addr = mlx_get_data_addr(cub.img.img, &cub.img.bits_per_pixel, &cub.img.line_length, &cub.img.endian);
+	mlx_hook(cub.ptr.win, 2, 1L<<0, ft_key_press, &cub.keyboard);
+	mlx_hook(cub.ptr.win, 3, 1L<<1, ft_key_release, &cub.keyboard);
+	ft_init_circle(&cub.circle);
+	mlx_loop_hook(cub.ptr.win, ft_main_loop, &cub);
+	mlx_loop(cub.ptr.mlx);
 	return (0);
 }
---
+++ mlx_test/main.h
@@ -0,0 +1,66 @@
+#ifndef MAIN_H
+# define MAIN_H
+# include "../minilibx_mms_20200219/mlx.h"
+# include "../minilibx_opengl_20191021/mlx.h"
+# include <stdlib.h>
+# include <math.h>
+
+# define	KEY_W		13
+# define	KEY_A		0
+# define	KEY_S		1
+# define	KEY_D		2
+# define	KEY_UP		126
+# define	KEY_DOWN	125
+# define	KEY_RIGHT	124
+# define	KEY_LEFT	123
+# define	KEY_ESC		53
+
+typedef struct	s_img
+{
+	void		*img;
+	char		*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}		t_img;
+
+typedef struct	s_ptr
+{
+	void		*mlx;
+	void		*win;
+}		t_ptr;
+
+
+typedef struct	s_keyboard
+{
+	int		up;
+	int		down;
+	int		left;
+	int		right;
+	int		esc;
+}		t_keyboard;
+
+typedef struct	s_mouse
+{
+	int		x;
+	int		y;
+	int		left;
+	int		right;
+}		t_mouse;
+
+typedef struct	s_circle
+{
+	int		x;
+	int		y;
+}		t_circle;
+
+typedef struct	s_cub
+{
+	t_img		img;
+	t_ptr		ptr;
+	t_keyboard	keyboard;
+	t_mouse		mouse;
+	t_circle	circle;
+}		t_cub;
+
+#endif
